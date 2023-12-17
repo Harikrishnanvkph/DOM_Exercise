07839758bd1c9d09f1fd01fda5d580abe03e4491
Harikrishnanvkph <harikrishnanvk0531@gmail.com>
2023-12-17 12:16:13 +0530

    dom

---
+++ script.js
@@ -0,0 +1,8 @@
+function foo(){
+    var firstName = document.getElementById("firstName").value;
+    var middleName = document.getElementById("middleName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+    console.log(`firstName : ${firstName}\nmiddleName : ${middleName}\nlastName : ${lastName}\n
+    email : ${email}`);
+}
